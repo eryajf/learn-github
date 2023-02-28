9da37a98c562deb99a5625e980ea5ec0208b2ec0
eryajf <eryajf@163.com>
2023-02-28 12:51:30 +0800

    测试

--- src/store/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue'
-import { createPinia } from 'pinia'
-
-export function setupStore(app: App) {
-  const store = createPinia()
-  app.use(store)
-}
-
-export * from './modules'
